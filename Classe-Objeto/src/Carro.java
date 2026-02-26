481a5a1bbe2f559a5cd8ffdb512bc861a63272f3
Isadora-Kairala <isadorarequer7191@gmail.com>
2026-02-26 09:28:00 -0300

    Método Construtor

--- Classe-Objeto/src/Carro.java
+++ Classe-Objeto/src/Carro.java
@@ -1,8 +1,9 @@
 public class Carro{
     // variáveis ou propriedades ou atributos
-    public String marca, modelo; // String é um tipo de dado do tipo classe => Maiúsculo
-    public float velocidade; //float é um tipo de dado primmitivo => Minúsculo
-    public boolean motor;  // boolean é um tipo de dado primitivo
+
+    public String marca, modelo;    // String é um tipo de dado do tipo classe => Maiúsculo
+    public float velocidade;       //float é um tipo de dado primmitivo => Minúsculo
+    public boolean motor;         // boolean é um tipo de dado primitivo
 
     //Metodo que converte o objeto em String
     public String toString(){
@@ -12,5 +13,15 @@ public class Carro{
                 " Velocidade: " + this.velocidade +
                 " Motor: " + this.motor;
     }
-    
+
+    //=> Metodo Construtor:
+    public Carro(String marca, String modelo, float velocidade, boolean motor){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidade = velocidade;
+        this.motor = motor;
+    }
+
+
+
 }
--- Classe-Objeto/src/TestaCarro.java
+++ Classe-Objeto/src/TestaCarro.java
@@ -1,38 +1,43 @@
 public class TestaCarro {
     public static void main(String[] args){
-        System.out.println("oi"); //jdk compila trazendo o resultado dessa linha por exemplo
+        //System.out.println("oi"); jdk compila trazendo o resultado dessa linha por exemplo
         //byte code => arquivo .class
         //jvm interpreta o .class, fica no jdk
         //compilado e interpretado ao mesmo tempo
         //instanciar objeto obj1
-        Carro obj1 = new Carro();
-        obj1.marca = "Fiat";
-        obj1.modelo = "Uno";
-        obj1.velocidade = 0;
-        obj1.motor = false;
-        System.out.println(obj1.toString()); //sout
-        // instanciei obj1
-
-        //obj pode chamar os metodos da classe que ele pertence !!
 
 
 
-        Carro obj2 = new Carro();
-        obj2.marca = "GM";
-        obj2.modelo = "Onix";
-        obj2.velocidade = 0;
-        obj2.motor = false;
-        System.out.println(obj2.toString());
 
-        // é tão bom entender :)
+            /*
+            Carro obj1 = new Carro();
+            obj1.marca = "Fiat";
+            obj1.modelo = "Uno";
+            obj1.velocidade = 0;
+            obj1.motor = false;
+            System.out.println(obj1.toString());
+            //sout
+             */
+            // instanciei obj1
 
-       /*
-       return " Marca: "  + marca +
-                " Modelo: " + modelo +
-                " Velocidade: " + velocidade +
-                " Motor: " + motor;
-    }
-        */
+            //obj pode chamar os metodos da classe que ele pertence !!
+
+
+            /*
+            Carro obj2 = new Carro();
+            obj2.marca = "GM";
+            obj2.modelo = "Onix";
+            obj2.velocidade = 0;
+            obj2.motor = false;
+            System.out.println(obj2.toString());
+
+             */
+
+
+        //objeto usando contrutor:
+        Carro carro = new Carro("Caoa Cherry", "Tiggo 8", 110, true);
+        System.out.println(carro.toString());
+
 
 
 
